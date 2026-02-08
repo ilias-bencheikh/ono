346a0ac61714b320649654ba0640adb17809d740
LIN leo <leo.lin@etu.u-paris.fr>
2026-02-08 17:14:39 +0100

    implémentations OCaml des fonctions externes sleep, print_cell, newline et clear_screen

--- src/lib/concrete_ono_module.ml
+++ src/lib/concrete_ono_module.ml
@@ -1,5 +1,8 @@
 type extern_func = Kdo.Concrete.Extern_func.extern_func
 
+(* Buffer global pour l'affichage *)
+let display_buffer = Buffer.create 4096
+
 let print_i32 (n : Kdo.Concrete.I32.t) : (unit, _) Result.t =
   Logs.app (fun m -> m "%a" Kdo.Concrete.I32.pp n);
   Ok ()
@@ -8,21 +11,50 @@ let print_i64 (n : Kdo.Concrete.I64.t) : (unit, _) Result.t =
   Logs.app (fun m -> m "%a" Kdo.Concrete.I64.pp n);
   Ok ()
 
-
-let random_i32  (_ : unit) : (Kdo.Concrete.I32.t,_) Result.t = 
-  let randint = Random.int32 Int32.max_int in 
+let random_i32 (_ : unit) : (Kdo.Concrete.I32.t, _) Result.t =
+  let randint = Random.int32 Int32.max_int in
   Ok (Kdo.Concrete.I32.of_int32 randint)
+
+(* Fonctions externes *)
+
+let sleep (milliseconds : Kdo.Concrete.I32.t) : (unit, _) Result.t =
+  let ms = Kdo.Concrete.I32.to_int milliseconds in
+  let seconds = Float.of_int ms /. 1000.0 in
+  Unix.sleepf seconds;
+  Ok ()
+
+let print_cell (cell_alive : Kdo.Concrete.I32.t) : (unit, _) Result.t =
+  let is_alive = Kdo.Concrete.I32.to_int cell_alive in
+  if is_alive <> 0 then Buffer.add_string display_buffer "🦊"
+  else Buffer.add_string display_buffer " ";
+  Ok ()
+
+let newline (_ : unit) : (unit, _) Result.t =
+  Buffer.add_char display_buffer '\n';
+  Ok ()
+
+let clear_screen (_ : unit) : (unit, _) Result.t =
+  (* Affiche le contenu du buffer *)
+  Buffer.output_buffer stdout display_buffer;
+  Out_channel.flush stdout;
+  (* Nettoyage du buffer *)
+  Buffer.clear display_buffer;
+  Ok ()
 
 
 let m =
   let open Kdo.Concrete.Extern_func in
   let open Kdo.Concrete.Extern_func.Syntax in
-  let functions = [ 
-    ("print_i32", Extern_func (i32 ^->. unit, print_i32));
-    ("print_i64", Extern_func (i64 ^->. unit, print_i64));
-    ("random_i32", Extern_func (unit ^->. i32 , random_i32))
-    ] in
-  {
-    Kdo.Extern.Module.functions;
-    func_type = Kdo.Concrete.Extern_func.extern_type;
+  let functions =
+    [ ("print_i32", Extern_func (i32 ^->. unit, print_i32))
+    ; ("print_i64", Extern_func (i64 ^->. unit, print_i64))
+    ; ("random_i32", Extern_func (unit ^->. i32, random_i32))
+    ; ("sleep", Extern_func (i32 ^->. unit, sleep))
+    ; ("print_cell", Extern_func (i32 ^->. unit, print_cell))
+    ; ("newline", Extern_func (unit ^->. unit, newline))
+    ; ("clear_screen", Extern_func (unit ^->. unit, clear_screen))
+    ]
+  in
+  { Kdo.Extern.Module.functions
+  ; func_type = Kdo.Concrete.Extern_func.extern_type
   }
